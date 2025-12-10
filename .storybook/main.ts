97d992d69266c1470f1ff5628aecc7899e28c0fa
charles olivier <chla84@yahoo.fr>
2025-12-10 14:13:20 +0100

    Add Storybook GitHub Pages deploy workflow

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,7 @@ const config: StorybookConfig = {
     name: '@storybook/react-webpack5',
     options: {},
   },
+  staticDirs: ['../public'],
   webpackFinal: async (config) => {
     if (config.resolve) {
       config.resolve.modules = [

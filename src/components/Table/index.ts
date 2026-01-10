9764b007915cb6003a0bcffa410e6940d6f1da43
charles olivier <chla84@yahoo.fr>
2026-01-10 11:17:05 +0100

    table

---
+++ src/components/Table/index.ts
@@ -0,0 +1,21 @@
+export {
+  Table,
+  TableHead,
+  TableBody,
+  TableFoot,
+  TableRow,
+  TableHeaderCell,
+  TableCell,
+  TableCaption,
+} from './Table'
+
+export type {
+  TableTypes,
+  TableHeadTypes,
+  TableBodyTypes,
+  TableFootTypes,
+  TableRowTypes,
+  TableHeaderCellTypes,
+  TableCellTypes,
+  TableCaptionTypes,
+} from './Table.types'

d06ece2af33c5386201d684835722d9cfc9172ea
charles olivier <chla84@yahoo.fr>
2026-01-14 12:03:51 +0100

    tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'tsup'
-import { sassPlugin, postcssModules } from 'esbuild-sass-plugin'
+import { postcssModules, sassPlugin } from 'esbuild-sass-plugin'
 import path from 'path'
+import { defineConfig } from 'tsup'
 
 const srcPath = path.resolve(__dirname, 'src')
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/styles/index.scss'],
   format: ['esm', 'cjs'],
   dts: true,
   tsconfig: 'tsconfig.build.json',
@@ -14,8 +14,15 @@ export default defineConfig({
   sourcemap: true,
   esbuildPlugins: [
     sassPlugin({
+      filter: /\.module\.scss$/,
       loadPaths: [srcPath],
       transform: postcssModules({}),
+      type: 'css-text',
+    }),
+    sassPlugin({
+      filter: /\.scss$/,
+      loadPaths: [srcPath],
+      type: 'css',
     }),
   ],
 })

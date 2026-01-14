bbc14e091e13acdb5864e27a957a48b791778fc1
charles olivier <chla84@yahoo.fr>
2026-01-14 14:47:05 +0100

    tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'tsup'
 const srcPath = path.resolve(__dirname, 'src')
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/styles/index.scss'],
+  entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   tsconfig: 'tsconfig.build.json',

b4345fd0cd9b0388e7f2a9028a911290c5b4c716
charles olivier <chla84@yahoo.fr>
2026-01-16 11:02:41 +0100

    stylelint (v1.0.53)

---
+++ stylelint.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/properties-alphabetical-order': true,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
+      },
+    ],
+    'selector-class-pattern': null,
+    'scss/dollar-variable-pattern': null,
+    'scss/at-mixin-pattern': null,
+    'declaration-empty-line-before': null,
+  },
+}

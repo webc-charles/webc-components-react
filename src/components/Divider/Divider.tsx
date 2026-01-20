d14f6028653be024edbd715835513c0254596cda
charles olivier <chla84@yahoo.fr>
2026-01-20 23:28:54 +0100

    fix: divider (v1.0.73)

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -5,7 +5,7 @@ import type { DividerTypes } from './Divider.types'
 export function Divider({
   ref,
   variant = 'solid',
-  spacing = 'md',
+  spacing = 'none',
   className,
   ...rest
 }: DividerTypes) {

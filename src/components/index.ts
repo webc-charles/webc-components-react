91a71f2f21851ccb9ce406a9b42fec01d373f069
charles olivier <chla84@yahoo.fr>
2026-01-29 15:48:00 +0100

    feat: section (v1.1.35)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,4 @@
-// Sections - Page-level layout components
-
-// Base - Atomic UI elements
 export * from './base'
-// Form - Form inputs and controls
 export * from './form'
-// Modules - Components that wrap/organize content
 export * from './modules'
 export * from './sections'
